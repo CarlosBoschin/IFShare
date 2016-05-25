4ef78b8c2f13065e3fb3088eba275dc85cb6ca40
Daniel Correa Lobato <dclobato@users.noreply.github.com>
2016-05-25 02:14:54 -0300

    Update createdb.sql
    Otimizando o esquema do banco de dados

--- createdb.sql
+++ createdb.sql
@@ -1,19 +1,16 @@
-CREATE TABLE 'arquivos' 
-  ( 
-     'sha1arquivo' VARCHAR(40) NOT NULL, 
-     'nome'        VARCHAR(128) NOT NULL, 
-     'tamanho'     BIGINT NOT NULL, 
-     'datahora'    DATETIME NOT NULL, 
-     'chunksize'   INT NOT NULL, 
-     'numchunks'   INT NOT NULL 
-  ) 
-engine = innodb; 
+CREATE TABLE `arquivos` (
+  `sha1arquivo` varchar(40) NOT NULL,
+  `nome` varchar(128) NOT NULL,
+  `tamanho` bigint(8) NOT NULL,
+  `datahora` datetime NOT NULL,
+  `chunksize` int(11) unsigned NOT NULL,
+  `numchunks` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`sha1arquivo`)
+) ENGINE=InnoDB
 
-CREATE TABLE 'partes' 
-  ( 
-     'sha1arquivo' VARCHAR(40) NOT NULL, 
-     'sha1parte'   VARCHAR(40) NOT NULL, 
-     'chunk'       INT NOT NULL, 
-     'onde'        VARCHAR(512) NOT NULL 
-  ) 
-engine = innodb; 
+CREATE TABLE `partes` (
+  `sha1arquivo` varchar(40) NOT NULL,
+  `sha1parte` varchar(40) NOT NULL,
+  `chunk` int(11) unsigned NOT NULL,
+  `onde` tinyint(1) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
