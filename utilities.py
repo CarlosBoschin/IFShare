c2b879e05355945d9154dcb023507aeac9981e96
Daniel Correa Lobato <daniel@lobato.org>
2016-05-31 15:45:31 -0300

    Healer + refatoracao dos utilitarios
    Mudanza na lógica do healer e refatoracao de algum utilitários

--- utilities.py
+++ utilities.py
@@ -10,7 +10,7 @@ import boto.sqs.message
 from boto.s3.connection import OrdinaryCallingFormat
 from boto.exception import S3ResponseError, S3CreateError
 from boto.s3.key import Key
-
+import mysql.connector
 
 def bestSize(tamanho):
     if tamanho < 1024:
@@ -50,14 +50,14 @@ def dumpMetadata(prop_dict):
 
 
 def readMetadata(fd, prop_dict):
-    prop_dict['arquivo'] = fd.readline().rstrip('\n')
+    prop_dict['arquivo'] = fd.readline().rstrip()
     prop_dict['tamanho'] = int(fd.readline())
-    prop_dict['criacao_do_arquivo'] = datetime.datetime.strptime(fd.readline().rstrip('\n'), "%Y%m%d%H%M%S")
-    prop_dict['hashgeral'] = fd.readline().rstrip('\n')
+    prop_dict['criacao_do_arquivo'] = datetime.datetime.strptime(fd.readline().rstrip(), "%Y%m%d%H%M%S")
+    prop_dict['hashgeral'] = fd.readline().rstrip()
     prop_dict['chunksize'] = int(fd.readline())
     prop_dict['numchunks'] = int(fd.readline())
     for chunk in range(0, prop_dict['numchunks']):
-        prop_dict['hashchunk' + str(chunk)] = fd.readline().rstrip('\n')
+        prop_dict['hashchunk' + str(chunk)] = fd.readline().rstrip()
 
     return
 
@@ -120,7 +120,7 @@ def createAndGetObjectStoreBucket(objectStoreDict, objectStoreConnection, debug=
                 print(
                     "[DEBUG: createAndGetObjectStoreBucket]: O bucket nao existe e houve erro na criacao dele. Vamos tentar pegar na marra")
             finally:
-                time.sleep(10)
+                time.sleep(15)
             k = Key(osb)
             k.key = '000-DoNotTouchMe-000'
             k.set_contents_from_string('Este bucket e usado pelo projeto IFShare que implementa um sistema de arquivos distribuidos. Tanto o bucket quanto os arquivos que estao aqui dentro sao criticos para o funcionamento do sistema. Sendo assim, caia fora e va fazer outra coisa...')
@@ -162,3 +162,20 @@ def createAndGetObjectStoreBucket(objectStoreDict, objectStoreConnection, debug=
             print("[DEBUG: createAndGetObjectStoreBucket]: Otimo! O bucket %s esta aqui e eh nosso" % (
                 objectStoreDict['BUCKET']))
     return (osb)
+
+def connectDB (configTree, debug = False):
+    if (debug):
+        print("-" * 80)
+        print("[DEBUG: connectDB]: Conectando ao banco de dados...")
+        print("-" * 80)
+    try:
+        db = mysql.connector.connect(user=configTree['USER'],
+                                     password=configTree['PASS'],
+                                     host=configTree['ENDPOINT'],
+                                     database=configTree['DATABASE'])
+    except mysql.connector.errors.InterfaceError:
+        print("%s%s" % ("+-----------+", "-" * 67))
+        print("| connectDB |  Banco de dados inacessivel")
+        print("%s%s" % ("+-----------+", "-" * 67))
+        sys.exit(1)
+    return (db)
